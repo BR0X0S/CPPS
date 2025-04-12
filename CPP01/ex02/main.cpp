731bde3802ff60d0f400742007321599a69be17f
oumondad <oumondad@e1r9p8.1337.ma>
2025-04-12 18:43:42 +0100

    start cpp01 ex02 done

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oumondad <oumondad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/12 18:21:40 by oumondad          #+#    #+#             */
+/*   Updated: 2025/04/12 18:42:52 by oumondad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main()
+{
+	std::string str;
+	str = "HI THIS IS BRAIN";
+
+	std::string *stringPTR;
+	stringPTR = &str;
+
+	std::string &stringREF = str;
+	
+	std::cout << "The memory address of the string variable : " << &str << std::endl;
+	std::cout << "The memory address held by stringPTR : " << &stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF : " << &stringREF << std::endl;
+
+	std::cout << "\nAnd then :\n" << std::endl;
+
+	std::cout << "The value of the string variable : " << str << std::endl;
+	std::cout << "The value pointed to by stringPTR : " << stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF : " << stringREF << std::endl;
+}
