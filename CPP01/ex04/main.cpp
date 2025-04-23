abaa7f8fbfd40d8816dd5c2d6ca13c76b75328e1
oumondad <oumondad@e1r9p8.1337.ma>
2025-04-23 16:30:12 +0100

    done ex04

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,70 +6,72 @@
 /*   By: oumondad <oumondad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 17:42:39 by oumondad          #+#    #+#             */
-/*   Updated: 2025/04/20 22:27:39 by oumondad         ###   ########.fr       */
+/*   Updated: 2025/04/23 16:16:20 by oumondad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.hpp"
 
-int count_world(std::string line)
+int ft_strlen(char *str)
 {
 	int i = 0;
-	int word = 0;
-
-	while (line[i])
-	{
-		while (line[i] && (line[i] == ' ' || (line[i] >= 9 && line[i] <= 13)))
-			i++;
-		if (line[i] && line[i] != ' ' && (line[i] < 9 || line[i] > 13))
-			word++;
-		while (line[i] && line[i] != ' ' && !(line[i] >= 9 && line[i] <= 13))
-			i++;
-	}
-	return (word);
+	while (str[i])
+		i++;
+	return (i);
 }
 
-void	replace(std::ifstream &file, std::string line, std::ofstream &newFile)
+void	replace(std::ifstream &file, std::string &line, std::ofstream &newFile, char *s1, char *s2)
 {
-		while (getline(file, line))
+	while (getline(file, line))
+	{
+		int i = 0;
+		int j;
+		while (line[i])
 		{
-			newFile << line;
+			j = 0;
+			while(line[i] == s1[j])
+			{
+				int x = i;
+				i++;
+				j++;
+				if (j == ft_strlen(s1))
+				{
+					newFile << s2;
+					break;
+				}
+				else
+					i = x;
+			}
+			newFile << line[i];
+			i++;
+		}
 			if (!file.eof())
 				newFile << "\n";
-			int i = 0;
-			while (i < count_world(line))
-			{
-				
-				i++;
-			}
-		}
+	}
 }
 
 int main(int ac, char **av)
 {
-	(void)ac;
-	std::cout << count_world(av[1]) << std::endl;
-	// if (ac == 4)
-	// {
-	// 	std::ifstream file(av[1]);
-	// 	if (!file)
-	// 	{
-	// 		std::cout << "ERROR : Can't Open File" << std::endl;
-	// 		return (1);
-	// 	}
-	// 	std::string line;
-	// 	std::string outf = av[1];
-	// 	outf += ".replace";
-	// 	std::ofstream newFile(outf.c_str());
-	// 	if (!newFile)
-	// 	{
-	// 		std::cout << "ERROR : Can't Creat newFile" << std::endl;
-	// 		return (1);
-	// 	}
-	// 	// now call replace() function
-	// 	replace(file, line, newFile);
-	// 	return (0);
-	// }
-	// std::cout << "ERROR : Wrong Parameters" << std::endl;
+	if (ac == 4)
+	{
+		std::ifstream file(av[1]);
+		if (!file)
+		{
+			std::cout << "ERROR : Can't Open File" << std::endl;
+			return (1);
+		}
+		std::string line;
+		std::string outf = av[1];
+		outf += ".replace";
+		std::ofstream newFile(outf.c_str());
+		if (!newFile)
+		{
+			std::cout << "ERROR : Can't Creat newFile" << std::endl;
+			return (1);
+		}
+		replace(file, line, newFile, av[2], av[3]);
+		return (0);
+	}
+	std::cout << "ERROR : Wrong Parameters" << std::endl;
 	return (0);
 }
