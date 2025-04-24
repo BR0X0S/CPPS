3123f468b49ae99669bfa59a8646dad16403a077
oumondad <oumondad@e1r9p8.1337.ma>
2025-04-24 17:53:36 +0100

    done ex04

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,53 +6,35 @@
 /*   By: oumondad <oumondad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/19 17:42:39 by oumondad          #+#    #+#             */
-/*   Updated: 2025/04/23 16:16:20 by oumondad         ###   ########.fr       */
+/*   Updated: 2025/04/24 17:52:32 by oumondad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.hpp"
 
-int ft_strlen(char *str)
+void	replace(std::ifstream &file, std::ofstream &newFile, std::string s1, std::string s2)
 {
-	int i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-void	replace(std::ifstream &file, std::string &line, std::ofstream &newFile, char *s1, char *s2)
-{
+	std::string line;
 	while (getline(file, line))
 	{
-		int i = 0;
-		int j;
-		while (line[i])
+		size_t pos = 0;
+		while ((pos = line.find(s1, pos)) != std::string::npos)
 		{
-			j = 0;
-			while(line[i] == s1[j])
-			{
-				int x = i;
-				i++;
-				j++;
-				if (j == ft_strlen(s1))
-				{
-					newFile << s2;
-					break;
-				}
-				else
-					i = x;
-			}
-			newFile << line[i];
-			i++;
+			line.erase(pos, s1.length());
+			line.insert(pos, s2);
+			pos += s2.length();
 		}
-			if (!file.eof())
-				newFile << "\n";
+		newFile << line;
+		if (!file.eof())
+			newFile << "\n";
 	}
 }
 
 int main(int ac, char **av)
 {
-	if (ac == 4)
+	std::string s1 = av[2];
+	std::string s2 = av[3];
+	if (ac == 4 && !s1.empty() && !s2.empty())
 	{
 		std::ifstream file(av[1]);
 		if (!file)
@@ -60,7 +42,6 @@ int main(int ac, char **av)
 			std::cout << "ERROR : Can't Open File" << std::endl;
 			return (1);
 		}
-		std::string line;
 		std::string outf = av[1];
 		outf += ".replace";
 		std::ofstream newFile(outf.c_str());
@@ -69,7 +50,7 @@ int main(int ac, char **av)
 			std::cout << "ERROR : Can't Creat newFile" << std::endl;
 			return (1);
 		}
-		replace(file, line, newFile, av[2], av[3]);
+		replace(file, newFile, s1, s2);
 		return (0);
 	}
 	std::cout << "ERROR : Wrong Parameters" << std::endl;
--- utilitis/test.cpp
+++ utilitis/test.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <string>
 
-using namespace std;
-
-int main(void)
+int main(int ac, char **av)
 {
-    
+	std::string s1 = av[1];
+	std::string s2 = av[2];
+	std::string line = "hellow baby yo are the1 bast wan in may layf";
+	size_t pos = line.find(s1);
+	if (pos != std::string::npos)
+	{
+		
+		std::cout << line.erase(pos, s1.length()) << std::endl;
+		std::cout << line.insert(pos, s2) << std::endl;
+		return 0;
+	}
+	std::cout << -1 << std::endl;
 }
+
