2bd0b036f78a3184ce6c02ec943ef9235ea58319
oumondad <oumondad@e1r8p9.1337.ma>
2025-06-05 19:22:34 +0100

    CPP03 done

--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: oumondad <oumondad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 18:05:06 by oumondad          #+#    #+#             */
-/*   Updated: 2025/06/02 21:50:45 by oumondad         ###   ########.fr       */
+/*   Updated: 2025/06/05 19:21:08 by oumondad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,11 @@
 
 int main()
 {
-	ClapTrap Robo("mok");
-	
-	Robo.attack("khaltk");
+	ClapTrap Robo("Ta_Mere");
+
+	Robo.attack("Ta_Tante");
+	Robo.takeDamage(9);
+	Robo.beRepaired(20);
+	Robo.takeDamage(11);
+	Robo.beRepaired(20);
 }
